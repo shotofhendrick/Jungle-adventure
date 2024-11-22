d565bcfa9840b874c86c93965183afa58b690ca9
shotofhendrick <jliubox03@gmail.com>
2024-11-21 21:39:33 -0500

    JungleAdventure

---
+++ Assets/scripts/portals.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portals : MonoBehaviour
+{
+   //portals: compare tag on trigger. shift camera to other map
+
+}
